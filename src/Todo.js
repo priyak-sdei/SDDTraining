edbb4081142d23f32415b93aa4a7238c7a9b49c6
gvishal14 <vishalgupta140701@gmail.com>
2023-05-01 10:52:31 +0530

    todo changes

--- src/Todo.js
+++ src/Todo.js
@@ -1,99 +1,144 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
-  const [editingId, setEditingId] = useState(null);
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  const addTodo = () => {
-    if (text.trim() === '') {
-      return;
+  const handleAddTask = () => {
+    if (taskName.trim() !== '') {
+      setTasks([...tasks, { name: taskName, description: taskDescription }]);
+      setTaskName('');
+      setTaskDescription('');
     }
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
-    setText('');
   };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+   const handleToggleTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
   };
-
-  const editTodo = (id, newText) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, text: newText };
-        } else {
-          return todo;
-        }
-      })
-    );
-    setEditingId(null);
+  const handleEditTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].editing = true;
+    setTasks(newTasks);
   };
-
-  const toggleCompleteTodo = (id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        } else {
-          return todo;
-        }
-      })
-    );
+  const handleSaveTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].name = taskName;
+    newTasks[index].description = taskDescription;
+    newTasks[index].editing = false;
+    setTasks(newTasks);
+    setTaskName('');
+    setTaskDescription('');
   };
-
-  const renderItem = ({ item }) => (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-      {editingId === item.id ? (
-        <TextInput
-          style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1 }}
-          value={item.text}
-          onChangeText={(text) => editTodo(item.id, text)}
-          onBlur={() => setEditingId(null)}
-          autoFocus={true}
-          selectTextOnFocus={true}
-        />
-      ) : (
-        <TouchableOpacity onPress={() => toggleCompleteTodo(item.id)}>
-          <Text style={{ textDecorationLine: item.completed ? 'line-through' : 'none' }}>{item.text}</Text>
-        </TouchableOpacity>
-      )}
-      <View style={{ flexDirection: 'row' }}>
-        {editingId !== item.id && (
-          <>
-            <Button title="Edit" onPress={() => setEditingId(item.id)} />
-            <View style={{ width: 5 }} />
-          </>
-        )}
-        <Button title="Delete" onPress={() => deleteTodo(item.id)} />
-      </View>
-    </View>
-  );
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
 
   return (
-    <View>
-      <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1 }}
-          placeholder="Add a new to-do item"
-          value={text}
-          onChangeText={(text) => setText(text)}
+          style={styles.input}
+          placeholder="Task name"
+          value={taskName}
+          onChangeText={setTaskName}
         />
-        <Button title="Add" onPress={addTodo} />
+        <TextInput
+          style={styles.input}
+          placeholder="Task description"
+          value={taskDescription}
+          onChangeText={setTaskDescription}
+        />
+        <Button
+          title="Add task"
+          onPress={handleAddTask}
+        />
       </View>
-      <FlatList
-        data={todos}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        extraData={editingId}
-      />
-      {/* git status */}
-      
+       <View style={styles.taskList}>
+        {tasks.map((task, index) => (
+          <View style={[styles.taskItem, task.completed && styles.completedTask]} key={index}>
+            {task.editing ? (
+              <View style={styles.editingContainer}>
+                <TextInput
+                  style={styles.editingInput}
+                  placeholder="Task name"
+                  value={taskName}
+                  onChangeText={setTaskName}
+                />
+                <TextInput
+                  style={styles.editingInput}
+                  placeholder="Task description"
+                  value={taskDescription}
+                  onChangeText={setTaskDescription}
+                />
+                <Button
+                  title="Save"
+                  onPress={() => handleSaveTask(index)}
+                />
+              </View>
+            ) : (
+              <>
+                <Text style={styles.taskName}>{task.name}</Text>
+                <Text style={styles.taskDescription}>{task.description}</Text>
+                <View style={styles.actionsContainer}>
+                  <Button
+                    title={task.completed ? 'Incomplete' : 'Complete'}
+                    onPress={() => handleToggleTask(index)}
+                  />
+                  <Button
+                    title="Edit"
+                    onPress={() => handleEditTask(index)}
+                  />
+                  <Button
+                    title="Delete"
+                    onPress={() => handleDeleteTask(index)}
+                  />
+                </View>
+              </>
+            )}
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
 
-export default TodoList;
 
-// todo list
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  taskList: {
+    flex: 1,
+  },
+  taskItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingVertical: 10,
+  },
+  taskName: {
+    fontWeight: 'bold',
+  },
+  taskDescription: {
+    color: '#666',
+  },
+});
+
+export default TodoList;
