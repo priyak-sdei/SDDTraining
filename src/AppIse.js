af338f19d2c48588cc787355da42462d12891fe5
gvishal14 <vishalgupta140701@gmail.com>
2023-07-19 10:18:51 +0530

    Axios

--- src/AppIse.js
+++ src/AppIse.js
@@ -1,49 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { myFetchPostRequest, myFetchPutRequest, myFetchPatchRequest, myFetchDeleteRequest } from './MyFetchApiRequests';
-import { MyAxiosGetRequest, myAxiosGetRequest } from './MyAxiosRequests';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  myFetchPostRequest,
+  myFetchPutRequest,
+  myFetchPatchRequest,
+  myFetchDeleteRequest,
+} from './MyFetchApiRequests';
+import {
+  MyAxiosGetRequest,
+  myAxiosGetRequest,
+  myAxiosPostRequest,
+} from './MyAxiosRequests';
 
 const AppIse = () => {
-    useEffect(() =>{
-       getData()
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    //for post request in axios
+    await myAxiosPostRequest({
+      title: 'sdsds',
+      desc: 'edwe',
+      id: 100,
+    })
+    .then(res => {
+      console.log(res);
+    }).catch(error =>{
+      console.log(error);
+    });
 
-    }, []);
-    const getData=async()=>{
-        await myAxiosGetRequest()
-        .then(res=>{
-          console.log(res.data);
-        })
-        .catch(error=>{
-          console.log(error);
-        });
+    // for get request
 
-        //  const res=await myFetchGetRequest();
-        // const res=await myFetchPutRequest({
-            // const data1 = {
-            //   title: 'fsfsfssf',
-            //   body:'dsdsdsds',
-            //   userId:1,
-            // };
-            // const data2 = {
-            //     title:'fsfsfsfs',
-            // };
-            // const res1 = await myFetchPostRequest(data1);
-            // const res = await myFetchPutRequest(1, data2);
+    // await myAxiosGetRequest()
+    //   .then(res => {
+    //     console.log(res.data);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
 
-            
-            // console.log("data",res);
+    //  const res=await myFetchGetRequest();
+    // const res=await myFetchPutRequest({
+    // const data1 = {
+    //   title: 'fsfsfssf',
+    //   body:'dsdsdsds',
+    //   userId:1,
+    // };
+    // const data2 = {
+    //     title:'fsfsfsfs',
+    // };
+    // const res1 = await myFetchPostRequest(data1);
+    // const res = await myFetchPutRequest(1, data2);
 
-            // const delRes = await myFetchPostRequest(2);
+    // console.log("data",res);
 
-            // console.log("data",delRes);
-                
-        };
+    // const delRes = await myFetchPostRequest(2);
+
+    // console.log("data",delRes);
+  };
   return (
     <View>
       <Text>AppIse</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AppIse
-
+export default AppIse;
